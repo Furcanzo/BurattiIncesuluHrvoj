b8759a0d8345315f276dd90b1a6ac22a9c7c2dce
Furcanzo <roberto.buratti@mail.polimi.it>
2021-01-28 15:53:35 +0100

    Minor fixes

--- ITD/src/main/java/com/example/demo/controllers/CustomerRoutes.java
+++ ITD/src/main/java/com/example/demo/controllers/CustomerRoutes.java
@@ -1,9 +1,11 @@
 package com.example.demo.controllers;
 
 import com.example.demo.entities.User;
-import com.example.demo.services.UserService;
+import com.example.demo.services.CustomerService;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,31 +14,31 @@ import java.util.List;
 public class CustomerRoutes {
 
     @Autowired
-    private UserService userService;
+    private CustomerService customerService;
 
     @Autowired
     private Gson gson;
 
     @GetMapping(path = "/test")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    public ResponseEntity<String> sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return ResponseEntity.status(HttpStatus.OK).body( "Hello " + name);
     }
 
     @GetMapping(path = "/test2")
     public String findUsers() {
-        List<User> a = userService.findAll();
+        List<User> a = customerService.findAll();
         return gson.toJson(a);
     }
 
     @GetMapping(path = "/test3")
     public String findUsersByName( @RequestParam(name = "name") String name) {
-        List<User> a = userService.findByName(name);
+        List<User> a = customerService.findByName(name);
         return gson.toJson(a);
     }
 
     @PostMapping(path = "/test4")
     public String create( @RequestBody User user) {
-        User a = userService.create(user);
+        User a = customerService.create(user);
         return gson.toJson(a);
     }
 
