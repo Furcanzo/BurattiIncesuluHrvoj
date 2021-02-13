9c8ea1d97363f853574d2bafd67320ccf49a175e
Ozan İncesulu <ozyinc@users.noreply.github.com>
2021-02-14 02:03:56 +0300

    test: add test for scanning tickets

--- ATD/CLup/cypress/integration/ticketScan.spec.ts
+++ ATD/CLup/cypress/integration/ticketScan.spec.ts
@@ -5,3 +5,33 @@
 *     updating the data contained into the store overview,
 *     and make sure that the flow of customers corresponds to that expected by the system (no one is able to enter before his turn).
 * */
+import QrScanner from 'qr-scanner';
+describe("Ticket Scanner", () => {
+    it("should scan a user in the store", () => {
+        cy.login(1, "manager", 3).as("newStore")
+        cy.login(1, "customer");
+        cy.get("@newStore").then(({storeName, ...rest}) => {
+            cy.get("a.store").contains(storeName).click();
+        });
+        cy.get("button#form-btn").click();
+        cy.get("img.qr-code").invoke("attr", "src").then((srcUrl) => {
+            return new Cypress.Promise((resolve, reject) => {
+                QrScanner.scanImage(srcUrl).then((value) => {
+                    resolve(value);
+                });
+            });
+        }).as("QRResult");
+        cy.login(1, "manager");
+        cy.get("#scan-btn").click();
+        cy.get("@QRResult").then((result) => {
+
+            cy.get("#ticket").type(result, {force: true});
+        })
+        cy.get("#form").submit();
+        cy.get(".toast").contains("Ticket valid, entrance allowed.").should("exist");
+
+        //Ticket valid, entrance allowed.
+        // cy.get("a[href='/explore']").click();
+
+    });
+})
