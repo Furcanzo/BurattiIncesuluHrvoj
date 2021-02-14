d3fb22009eca9eda2c6bcb3425643384a54e9fc2
Ozan İncesulu <ozyinc@users.noreply.github.com>
2021-02-14 12:17:46 +0300

    test: add more tests needing ticket scanning

--- ATD/CLup/cypress/integration/ticketScan.spec.ts
+++ ATD/CLup/cypress/integration/ticketScan.spec.ts
@@ -6,32 +6,91 @@
 *     and make sure that the flow of customers corresponds to that expected by the system (no one is able to enter before his turn).
 * */
 import QrScanner from 'qr-scanner';
+
 describe("Ticket Scanner", () => {
-    it("should scan a user in the store", () => {
-        cy.login(1, "manager", 3).as("newStore")
-        cy.login(1, "customer");
+    const generateTicketSrc = () => {
         cy.get("@newStore").then(({storeName, ...rest}) => {
             cy.get("a.store").contains(storeName).click();
         });
         cy.get("button#form-btn").click();
-        cy.get("img.qr-code").invoke("attr", "src").then((srcUrl) => {
+        return cy.get("img.qr-code").invoke("attr", "src").then((srcUrl) => {
             return new Cypress.Promise((resolve, reject) => {
                 QrScanner.scanImage(srcUrl).then((value) => {
                     resolve(value);
                 });
             });
-        }).as("QRResult");
-        cy.login(1, "manager");
+        })
+    };
+    const validateTicket = (alias) => {
         cy.get("#scan-btn").click();
-        cy.get("@QRResult").then((result) => {
-
+        cy.get(alias).then((result) => {
             cy.get("#ticket").type(result, {force: true});
         })
         cy.get("#form").submit();
+    }
+    it("should scan a user in the store", () => {
+        cy.login(1, "manager", 3).as("newStore")
+        cy.login(1, "customer");
+        generateTicketSrc().as("QRResult");
+        cy.login(1, "manager");
+        validateTicket("@QRResult");
         cy.get(".toast").contains("Ticket valid, entrance allowed.").should("exist");
+        cy.get("h2").contains("Inside").siblings("span.report").contains("1").should("exist");
+        cy.get("h2").contains("In line").siblings("span.report").contains("0").should("exist");
+    });
 
-        //Ticket valid, entrance allowed.
-        // cy.get("a[href='/explore']").click();
+    it("should scan a user out of the store", () => {
+        cy.login(1, "manager", 3).as("newStore")
+        cy.login(1, "customer");
+        generateTicketSrc().as("QRResult");
+        cy.login(1, "manager");
+        validateTicket("@QRResult");
+        validateTicket("@QRResult");
+        cy.get(".toast").contains("Ticket valid, exit allowed.").should("exist");
+        cy.get("h2").contains("In line").siblings("span.report").contains("0").should("exist");
+        cy.get("h2").contains("Inside").siblings("span.report").contains("0").should("exist");
 
     });
+
+    it("should not allow entrance of more than the limit", () => {
+        cy.login(1, "manager", 1).as("newStore")
+        cy.login(1, "customer");
+        generateTicketSrc().as("QRResult1");
+        cy.login(2, "customer");
+        generateTicketSrc().as("QRResult2");
+        cy.login(1, "manager");
+        validateTicket("@QRResult1");
+        validateTicket("@QRResult2");
+        cy.get(".toast").contains("The store is full. No information about the validity of the ticket.").should("exist");
+        cy.get("h2").contains("In line").siblings("span.report").contains("1").should("exist");
+        cy.get("h2").contains("Inside").siblings("span.report").contains("1").should("exist");
+
+
+    });
+
+    it("should prevent already exited tickets from re-exiting", () => {
+        cy.login(1, "manager", 1).as("newStore")
+        cy.login(1, "customer");
+        generateTicketSrc().as("QRResult");
+        cy.login(1, "manager");
+        validateTicket("@QRResult");
+        validateTicket("@QRResult");
+        validateTicket("@QRResult");
+        cy.get(".toast").contains("No ticket found. Ticket is invalid, access not allowed.").should("exist");
+    });
+
+    it("should prevent people from entering before their turn", () => {
+        cy.login(1, "manager", 1).as("newStore");
+        cy.login(1, "customer");
+        generateTicketSrc().as("QRResult1");
+        cy.login(2, "customer");
+        generateTicketSrc().as("QRResult2");
+        cy.login(1, "manager");
+        validateTicket("@QRResult2");
+        cy.get(".toast").contains("This ticket is not the head of the queue, entrance not allowed.").should("exist");
+
+        //
+    })
+
+
 })
