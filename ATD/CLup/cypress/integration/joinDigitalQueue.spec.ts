f84fbcc9eff942584930dc08fdc9a29c6e8719dd
Ozan İncesulu <ozyinc@users.noreply.github.com>
2021-02-14 19:50:14 +0300

    Added new test to joinDigitalQueue.spec.ts Added comments and mapped tests to requirements

--- ATD/CLup/cypress/integration/joinDigitalQueue.spec.ts
+++ ATD/CLup/cypress/integration/joinDigitalQueue.spec.ts
@@ -10,13 +10,11 @@
 * */
 
 describe("JoinDigitalQueue", () => {
-    before(()=>{
+    it("R7 - A clupper is able to join a queue. ", () => {
+        // OK
+        cy.login(1, "manager");
         cy.login(1, "customer");
-    });
-    beforeEach(()=>{
         cy.visit("/explore");
-    });
-    it("Select a supermarket", () => {
         const firstStore = cy.get("a.store").first();
         firstStore.within(()=>{
             cy.get("div.store-info>h2").invoke('text').as("storeName");
@@ -33,32 +31,40 @@ describe("JoinDigitalQueue", () => {
 
     });
 
-    // R6 The system is able to generate a new ticket after receiving a request
-
-    // R8 The system is able to insert a ticket into a store queue
-
-    // R16 A clupper is able to retrieve a previously obtained ticket
-    it("Join the queue", ()=>{
-        cy.login(1, "manager");
-        cy.login(1, "customer");
-        cy.get("a.store").last().click();
+    it("R8 - The system is able to insert a ticket into a store queue", ()=>{
+        // OK
+        // Even though the correct sentence for error should be "1 user in the line" :)
+        cy.login(6, "manager").as("newStore");
+        cy.login(6, "customer");
+        cy.get("@newStore").then((newStore) => {
+            cy.get("a.store").contains(newStore.storeName).click();
+        });
         cy.get("button#form-btn").click();
-        cy.get("body").then(($el) => {
-            cy.wrap($el.html());
-        }).as("ticket");
         cy.get("a[href='/explore']").click();
-        cy.login(1, "customer");
-        cy.get("a[href='/explore/queue']").click();
-        cy.get("@ticket").then((tic)=>{
-            cy.get("body").then(($el) => {
-                cy.wrap($el.html());
-            }).should("eq",tic);
+        cy.get("@newStore").then((newStore) => {
+            cy.get("a.store").contains(newStore.storeName).siblings().contains("1 users in line").should("exist");
         });
     });
 
-    // R9 The system is able to remove a ticket from a store queue
+    it("R16 - A clupper is able to retrieve a previously obtained ticket", () => {
+        // OK
+        cy.login(7, "manager").as("newStore");
+        cy.login(7, "customer");
+        cy.get("@newStore").then((newStore) => {
+            cy.get("a.store").contains(newStore.storeName).click();
+        });
+        cy.get("button#form-btn").click();
+        cy.get("body").invoke("html").as("ticket");
+        cy.get("a[href='/explore']").click();
+        cy.login(7, "customer");
+        cy.get("a[href='/explore/queue']").click();
+        cy.get("@ticket").then((tic)=>{
+            cy.get("body").invoke("html").should("eq", tic);
+        });
+    })
 
-    it.only("Leave the queue", ()=>{
+    it("R9 - The system is able to remove a ticket from a store queue", ()=>{
+        // OK
         cy.login(2, "manager").as("newStore");
         cy.login(2, "customer");
         cy.get("@newStore").then((newStore) => {
@@ -74,6 +80,23 @@ describe("JoinDigitalQueue", () => {
 
     });
 
-    // R7 A clupper is able to join at most one queue at any time (for any store)
+    it("R7 - A clupper is able to join at most one queue at any time (for any store)", () => {
+        // OK
+        cy.login(4, "manager").as("firstStore");
+        cy.login(5, "manager").as("secondStore");
+        cy.login(3, "customer");
 
+        cy.get("@firstStore").then(({storeName, ...rest}) => {
+            cy.get(".store").contains(storeName).click();
+        });
+        cy.get("button#form-btn").click();
+        cy.get("a[href='/explore']").click();
+
+        cy.get("@secondStore").then(({storeName, ...rest}) => {
+            cy.get(".store").contains(storeName).click();
+        });
+        cy.get(".btn.disable").should("exist");
+        cy.get(".btn.disable").click();
+        cy.get(".toast").contains("You can be in one queue at a time.").should("exist");
+    })
 });
